6367cc1f96816d7929f02bcd861f0c3c951ed217
Mohamed Coulibali <konoufo@hotmail.fr>
2021-10-12 00:23:38 -0400

    FCT: affiche nb total de fichiers et taille des buckets

--- s3cli.py
+++ s3cli.py
@@ -1,9 +1,19 @@
 import argparse
+
 import bucket
-import itertools
 
 
 DATE_FORMAT = '%Y-%m-%dT%H:%M'
+
+
+class SIZE_UNITS:
+    KB = 1024
+    MB = 1024 ** 2
+    GB = 1024 ** 3
+
+
+def convert_size_to(size_in_octets, size_unit):
+    return round(size_in_octets / size_unit, 2)
 
 
 class S3Cli:
@@ -18,12 +28,18 @@ class S3Cli:
         for i in self.s3_controller.list():
             creation_date = i.creation_date.strftime(DATE_FORMAT)
             last_modified = None
-            for last in i.object_versions.filter():
-                print(last)
-                last_modified = last.last_modified
-            last_modified = last_modified.strftime(DATE_FORMAT)
-            print([i.name, len(list(i.objects.all())), creation_date, last_modified])
-        else:
+            count = 0
+            size = 0
+            for last in bucket.objects.all():
+                print('storage_class', last.storage_class)
+                count += 1
+                size += last.size
+                last_modified = min(last_modified, last.last_modified) if last_modified is not None else last.last_modified
+            if last_modified is not None:
+                last_modified = last_modified.strftime(DATE_FORMAT)
+            size = convert_size_to(size, SIZE_UNITS.KB)
+            print([bucket.name, count, size, creation_date, last_modified])
+        if bucket is None:
             print('Aucun bucket à afficher.')
 
 
